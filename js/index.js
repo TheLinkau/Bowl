7357745f4ececddc8f758bbead6a01a23bbecb1a
nathan <nathan.merigeau@gmail.com>
2022-01-17 11:46:35 +0100

    ajout de tests

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    fs.readFile('./menu.html', function(err,data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
-}).listen(8080);
+const http = require("http");
+const finalhandler = require("finalhandler");
+const serveStatic = require("serve-static");
+ 
+// Crée une fonction middleware qui servira le contenu du dossier courant de façon statique
+const serve = serveStatic("./");
+ 
+// Création du serveur
+const server = http.createServer(function(req, res) {
+    serve(req, res, finalhandler(req, res)); // Traitement de la requête par le middleware
+});
+ 
+// Lancement
+server.listen(8080, function() {
+    console.log("Static server is up on http://localhost:8080");
+});
